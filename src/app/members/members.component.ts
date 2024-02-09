1da4bf079a0d0811a9443d6eb72a475a4ab2196b
ManglaN <mangla.nagar@numeroeins.com>
2024-02-09 18:29:29 +0530

    Member Component Created

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
@@ -7,10 +8,71 @@ import { Component, AfterViewInit } from '@angular/core';
 })
 export class MembersComponent implements AfterViewInit{
 
+  memberList = [
+    {
+      image:'../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Apoorva Pandey",
+      icon:'../../assets/images/icons/lock_member.svg',
+      contactNo:"Profile Lock",
+      emailIcon:'../../assets/images/icons/lock_member.svg',
+      emailDetails:"Profile Lock",
+      designation:"Corporate Lawyer",
+      branch:"Pune"
+    },
+    {
+      image:'../../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Bhushan Singh",
+      icon:'../../assets/images/icons/call_member.svg',
+      contactNo:"+91-456-789-4560",
+      emailIcon:'../../assets/images/icons/mail_member.svg',
+      emailDetails:"bhushansingh1@gmail.com",
+      designation:"Civil Lawyer",
+      branch:"Goa"     
+    },
+    {
+      image:'../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Kunal Thakre",
+      icon:'../../assets/images/icons/call_member.svg',
+      contactNo:"+91-456-789-4560",
+      emailIcon:'../../assets/images/icons/mail_member.svg',
+      emailDetails:"kunalthakre1@gmail.com",
+      designation:"Employement Lawyer",
+      branch:"Mumbai"     
+    },
+    {
+      image:'../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Archana Upadhyaya",
+      icon:'../../assets/images/icons/lock_member.svg',
+      contactNo:"Profile Lock",
+      emailIcon:'../../assets/images/icons/lock_member.svg',
+      emailDetails:"Profile Lock",
+      designation:"Criminal Lawyer",
+      branch:"Pune"     
+    },
+    {
+      image:'../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Manika Da",
+      icon:'../../assets/images/icons/lock_member.svg',
+      contactNo:"Profile Lock",
+      emailIcon:'../../assets/images/icons/mail_member.svg',
+      emailDetails:"manikada32@gmail.com",
+      designation:"Civil Lawyer",
+      branch:"Pune"     
+    },
+    {
+      image:'../../assets/images/image/anurag_goyal.jpg',  
+      memberName:"Kunal Thakre",
+      icon:'../../assets/images/icons/call_member.svg',
+      contactNo:"+91-456-789-4560",
+      emailIcon:'../../assets/images/icons/mail_member.svg',
+      emailDetails:"kunalthakre1@gmail.com",
+      designation:"Employement Lawyer",
+      branch:"Mumbai"     
+    },
+    
+  ];
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngAfterViewInit() {
     let element = document.getElementById('modalButton') as HTMLElement;
